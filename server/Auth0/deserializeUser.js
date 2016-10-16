3c010f74c377ff678d08cd3b242e651acb6853b4
Jonathan Gill <mrjonathangill@gmail.com>
2016-10-16 21:07:55 +0200

    Better user-name generator
    It crudely picks whatever's first in the email adress!

--- server/Auth0/deserializeUser.js
+++ server/Auth0/deserializeUser.js
@@ -1,4 +1,11 @@
+const email = require('email-validator')
+
 module.exports = function (user, done) {
   // Wow!
+  let name = user.userName
+  if(email.validate(name)) {
+    let newDisplayName = name.slice(0, name.indexOf('@')).replace(/(\.|\-|\_)/g, ' ')
+      user.userName = newDisplayName
+  }
   done(null, user)
 }
